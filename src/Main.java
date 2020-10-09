2324c778e86ab83c654e7be0ebae5dd71e537efb
svensaal <Matiteab2019>
2020-10-09 09:46:16 +0300

    Exercise 16

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,10 @@ public class Main {
         System.out.print("Type a number: ");
         int number = Integer.parseInt(reader.nextLine());
 
-        if (number > 0) {
-            System.out.println("The number is positive.");
-        } else {
-            System.out.println("The number is negative.");
-        }
-
+        if ( number % 2 == 0 )
+            System.out.println("Entered " + number + " is even");
+        else
+            System.out.println("Entered " + number + " is odd");
     }
 }
     
