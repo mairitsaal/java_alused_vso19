b61b84175248812527434a13f2f0e9d4fd9c5817
svensaal <Matiteab2019>
2020-10-09 17:48:54 +0300

    Exercise 22

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Type a year: ");
-        int year = Integer.parseInt(reader.nextLine()); // Reads a string variable from the keyboard and transfers it to an integer
-
-        if (year % 4 ==0 || year % 100 == 0 &&  year % 400 == 0) {
-            System.out.println("The year is a leap year.");
-        } else {
-            System.out.println("The year is not a leap year.");
+        System.out.print("Type the password: ");
+        String password = reader.nextLine();
+        while(!password.equals("carrot")){
+            System.out.println("Wrong!");
+            System.out.print("Type the password: ");
+            password = reader.nextLine();
         }
+        System.out.println("Right!");
+        System.out.println();
+        System.out.println("The secret is: jryy qbar!");
 
 
     }
