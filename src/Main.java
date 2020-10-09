847f2cbb566f49f4c40165f9a17dc86ee40b2dd9
svensaal <Matiteab2019>
2020-10-09 18:29:55 +0300

    Exercise 30

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int number = 2;
-        while (number <= 100) {
-            if (number % 2 == 0){
-                System.out.println(number);
-            }
-            number ++;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Call the number:  ");
+        int upperNumber = Integer.parseInt(reader.nextLine());
+        int currentNumber = 1;
+        while (currentNumber <= upperNumber) {
+            System.out.println(currentNumber);
+            currentNumber++;
         }
-
     }
 }
     
