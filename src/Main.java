d2b04a216ef0a70c77d94fd8aad613b363630446
svensaal <Matiteab2019>
2020-10-09 09:06:11 +0300

    Exercise 1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Type a number: ");
-        int firstNumber = Integer.parseInt(reader.nextLine());
-        System.out.println("Type another number: ");
-        int secondNumber = Integer.parseInt(reader.nextLine());
-
-        double result = (double)firstNumber / secondNumber;
-        System.out.println("Division: " + firstNumber + "/" + secondNumber + "=" + result);
+        System.out.print("Mairit Saal");
     }
 }
+    
