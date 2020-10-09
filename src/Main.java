7fe0cfbe26f383884cd131edff7224f095078565
svensaal <Matiteab2019>
2020-10-09 09:35:48 +0300

    Exercise 14

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.print("Type a number: ");
+        int number = Integer.parseInt(reader.nextLine());
 
-
-        System.out.print("Type the radius: ");
-        int radius = Integer.parseInt(reader.nextLine());
-        System.out.println("Circumference of the circle: " + 2 * Math.PI * radius);
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is negative.");
+        }
 
     }
 }
