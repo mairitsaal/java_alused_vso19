430f22d6fa44da7b67ab19bda52b20d2c60922c7
svensaal <Matiteab2019>
2020-10-09 10:15:35 +0300

    Exercise 18

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type a number: ");
+        System.out.print("Type the points [0-60]: ");
         int number = Integer.parseInt(reader.nextLine());
 
-        if ( number % 2 == 0 )
-            System.out.println("Entered " + number + " is even");
-        else
-            System.out.println("Entered " + number + " is odd");
+        if ( number >= 20 && number <= 29) {
+            System.out.println("Failed");
+        } else if (number >= 30 && number <= 34) {
+            System.out.println("Grade 1");
+        } else if (number >= 35 && number <= 39) {
+            System.out.println("Grade 2");
+        } else if (number >= 40 && number <= 44) {
+            System.out.println("Grade 3");
+        } else if (number >= 45 && number <= 49) {
+            System.out.println("Grade 4");
+        } else if (number >= 50 && number <= 60) {
+            System.out.println("Grade 5");
+        }
+
     }
 }
     
