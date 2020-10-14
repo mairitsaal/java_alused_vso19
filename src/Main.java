a5d41a9e2956a66aab9dfa3d57902faf4107557f
svensaal <Matiteab2019>
2020-10-14 13:29:19 +0300

    Exercise 36

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Enter the number:  ");
-        int number = Integer.parseInt(reader.nextLine());
-        int checkNumber = 1;
+        System.out.print("Type numbers:  ");
+
         int sum = 0;
-        while ( checkNumber <= number){
-            System.out.println(checkNumber);
-            sum+= checkNumber;
-            checkNumber++;
+        int count = 0;
+        double average = 0.0;
+        int odd = 0;
+        int notOdd = 0;
+
+        while(true) {
+            int numbers = Integer.parseInt(reader.nextLine());
+            if (numbers == -1){
+                break;
+            }
+            sum += numbers;
+            count++;
+            average = (double)sum / count;
+            if(numbers % 2 == 0) {
+             odd++;
+            } else {
+             notOdd++;
+            }
         }
-        System.out.println("Sum is " + sum);
+        System.out.println("Thank you and see you later! ");
+        System.out.println("Sum is: " + sum);
+        System.out.println("How many numbers entered: " + count);
+        System.out.println("Numbers average: " + average);
+        System.out.println("Odd numbers : " + odd);
+        System.out.println("NotOdd numbers : " + notOdd);
+
     }
 }
 
