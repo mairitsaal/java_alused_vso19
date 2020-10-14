bd220728e9b651374efc2adebeae370d9cf93543
svensaal <Matiteab2019>
2020-10-14 12:57:16 +0300

    Exercise 32

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Call the number:  ");
-        int upperNumber = Integer.parseInt(reader.nextLine());
-        int currentNumber = 1;
-        while (currentNumber <= upperNumber) {
-            System.out.println(currentNumber);
-            currentNumber++;
+        System.out.print("Enter the number:  ");
+        int number = Integer.parseInt(reader.nextLine());
+        int checkNumber = 1;
+        int sum = 0;
+        while ( checkNumber <= number){
+            System.out.println(checkNumber);
+            sum+= checkNumber;
+            checkNumber++;
         }
+        System.out.println("Sum is " + sum);
     }
 }
+
+
     
