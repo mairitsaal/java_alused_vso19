dd382654647c12e1e18772601f11f9bd4540593d
svensaal <Matiteab2019>
2020-10-14 15:23:00 +0300

    Exercise 39-2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void printRectangle(int width, int height) {
+        // 39.3
+        int count = 0;
+        while (count < width && count < height) {
+
+            printStars(width);
+            System.out.print("*");
+            count++;
+
+        }
+
+    }
         private static void printStars(int amount) {
             // you can print one star with the command
             //System.out.print("*");
@@ -12,10 +24,17 @@ public class Main {
             System.out.println();
         }
 
+
+
+
+
         public static void main(String[] args) {
             printStars(5);
             printStars(3);
             printStars(9);
+            printRectangle(17, 3);
         }
+
+
 }
 
