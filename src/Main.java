34f8d7ea01936ea0d471520f2cd199fbfda877cf
mairit.saal <mairit.saal@khk.ee>
2020-11-04 15:17:15 +0200

    Exercise 32

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Create a program that calculates the sum 1+2+3+…+n where n is a number entered by the user.
         Scanner reader = new Scanner(System.in);
-        System.out.print("First: ");
-        int firstNumber = Integer.parseInt(reader.nextLine());
-        System.out.print("Second: ");
+        System.out.print("Your number: ");
+        int number = Integer.parseInt(reader.nextLine());
+
        // String number = reader.nextLine(); // Reads a line of input from the user and assigns it
         //     to the variable called name
+        int count = 0; //loen arve, alustan 0-st
+        int sum = 0; //arvude summa
 
-        int secondNumber = Integer.parseInt(reader.nextLine());
-
-        if (firstNumber <= secondNumber){
-            System.out.println(firstNumber);
-            firstNumber++;
-        }else {
-                System.out.println("First: " + firstNumber + "Second: " + secondNumber);
-            }
+        while (count <= number) { //loenda kuni sisestatud numbrini
+            sum += count; //summale lisad uue loendatud arvu juurde
+            ++count; //suurendad ühe võrra
+        }
+        System.out.println(sum);
         }
 }
