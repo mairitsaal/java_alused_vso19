9c01a60587e1074dbb2cd3d64e86cee4b08a3fdc
mairit.saal <mairit.saal@khk.ee>
2020-11-04 12:17:36 +0200

    Ex 31

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void printRectangle(int width, int height) {
-        // 39.3
-        int count = 0;
-        while (count < width && count < height) {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("First: ");
+        int firstNumber = Integer.parseInt(reader.nextLine());
+        System.out.print("Second: ");
+       // String number = reader.nextLine(); // Reads a line of input from the user and assigns it
+        //     to the variable called name
 
-            printStars(width);
-            System.out.print("*");
-            count++;
+        int secondNumber = Integer.parseInt(reader.nextLine());
 
-        }
-
-    }
-        private static void printStars(int amount) {
-            // you can print one star with the command
-            //System.out.print("*");
-            int howManyTimes = 1;
-            while (howManyTimes <= amount) {
-                System.out.print("*");
-                howManyTimes++;
+        if (firstNumber <= secondNumber){
+            System.out.println(firstNumber);
+            firstNumber++;
+        }else {
+                System.out.println("First: " + firstNumber + "Second: " + secondNumber);
             }
-            System.out.println();
         }
-
-
-
-
-
-        public static void main(String[] args) {
-            printStars(5);
-            printStars(3);
-            printStars(9);
-            printRectangle(17, 3);
-        }
-
-
 }
-
