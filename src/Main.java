d6a9ef44a1a2f295e495307136b2a63ae211ac62
mairit.saal <mairit.saal@khk.ee>
2020-11-05 12:24:22 +0200

    Exercise 44

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,21 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner reader = new Scanner(System.in);
+        //Scanner reader = new Scanner(System.in);
+        //userNumber = Integer.parseInt(reader.nextLine());
+        int answer =  least(5, 2);
+        System.out.println("Least: " + answer);
 
-        int numberDrawn = drawNumber();
-
-        int count = 0;
-        int userNumber = 0;
-
-        while (true) {
-            System.out.print("Guess a number: ");
-            userNumber = Integer.parseInt(reader.nextLine());
-            if (userNumber == numberDrawn){
-                count++;
-                System.out.println("Congratulations, your guess is correct!" + " Guesses made: "+ count);
-                break;
-            } else if(userNumber < numberDrawn) {
-                count++;
-                System.out.println("The number is greater " + userNumber + ", guesses made: "+ count);
-            } else {
-                count++;
-                System.out.println("The number is lesser " + userNumber + ", guesses made: "+ count);
-            }
+    }
+    public static int least(int number1, int number2) {
+        if(number1<number2){
+            return number1;
+        }else {
+            return number2;
         }
-        }
+        // do not print anything inside the method
 
-    private static int drawNumber() {
-        return new Random().nextInt(101);
+        // method needs a return in the end
     }
 }
 
