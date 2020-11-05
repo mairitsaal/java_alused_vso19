b0aae80d4c96840a857576690f840c79c5572b80
mairit.saal <mairit.saal@khk.ee>
2020-11-05 10:59:10 +0200

    Exercise 39-1

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner reader = new Scanner(System.in);
-        System.out.print("How many: ");
-        int numbers = Integer.parseInt(reader.nextLine());
-        int i = 1;//it should be i = 1; and not i = 0;
-        while (i <= numbers) {
-            i++;
-            printText();
-        }
-    }
+        //Scanner reader = new Scanner(System.in);
+        //System.out.print("How many: ");
+        //int numbers = Integer.parseInt(reader.nextLine());
+        //int i = 1;//it should be i = 1; and not i = 0;
+        //while (i <= numbers) {
+          //  i++;
 
-    public static void printText() {
-        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+        printStars(5);
+        System.out.println();
+        printStars(3);
+        System.out.println();
+        printStars(9);
+        }
+
+
+    private static void printStars(int amount) {
+        // you can print one star with the command
+        for (int i = 0; i < amount; i++) {
+            System.out.print("*");
+        }
+        // call this command amount times
+
     }
         }
 
