00e2385d5973221a9055b8e12e7eec06557eeb78
mairit.saal <mairit.saal@khk.ee>
2020-11-05 11:55:04 +0200

    Exercise 41-3

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,22 @@ public class Main {
 
         int numberDrawn = drawNumber();
 
+        int count = 0;
         int userNumber = 0;
 
         while (true) {
             System.out.print("Guess a number: ");
             userNumber = Integer.parseInt(reader.nextLine());
             if (userNumber == numberDrawn){
-                System.out.println("Congratulations, your guess is correct!");
+                count++;
+                System.out.println("Congratulations, your guess is correct!" + " Guesses made: "+ count);
                 break;
             } else if(userNumber < numberDrawn) {
-                System.out.println("The number is greater, guesses made: " + userNumber);
+                count++;
+                System.out.println("The number is greater " + userNumber + ", guesses made: "+ count);
             } else {
-                System.out.println("The number is lesser, guesses made: " + userNumber);
+                count++;
+                System.out.println("The number is lesser " + userNumber + ", guesses made: "+ count);
             }
         }
         }
