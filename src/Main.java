00b4c223680d036f6de1d9321abe5a97f6560988
mairit.saal <mairit.saal@khk.ee>
2020-11-05 09:52:30 +0200

    Exercise 36

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // The sum between two numbers
+
         Scanner reader = new Scanner(System.in);
-        System.out.print("First number: ");
-        int firstNumber = Integer.parseInt(reader.nextLine());
-        System.out.print("Second number: ");
-       // String number = reader.nextLine(); // Reads a line of input from the user and assigns it
+        System.out.print("Type numbers: ");
+
+        // String number = reader.nextLine(); // Reads a line of input from the user and assigns it
         //     to the variable called name
-        int secondNumber = Integer.parseInt(reader.nextLine());
+
 
         int sum = 0;
+        int count = 0;
+        double average = 0.0;
+        int odd = 0;
+        int notOdd = 0;
 
-        while (firstNumber <=  secondNumber) {
-            sum += firstNumber; // sum = sum + firstNumber
-            firstNumber++;
+        while (true) {
+            int numbers = Integer.parseInt(reader.nextLine());
+            if (numbers == -1) {
+                break;
+            }
+            sum += numbers;
+            count++;
+            average = (double) sum / count;
+            if (numbers % 2 == 0) {
+                odd++;
+            } else {
+                notOdd++;
+            }
         }
-        System.out.println(sum);
+            System.out.println("Thank you and see you later! ");
+            System.out.println("Sum is: " + sum);
+            System.out.println("How many numbers entered: " + count);
+            System.out.println("Numbers average: " + average);
+            System.out.println("Odd numbers : " + odd);
+            System.out.println("NotOdd numbers : " + notOdd);
+
         }
-}
+    }
+
