e63042cd5b7fe9b753bdcd221eb3d965cda8687f
mairit.saal <mairit.saal@khk.ee>
2020-11-07 00:14:59 +0200

    Ex 61

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,18 @@ import java.util.*;
 
 
 public class Main {
-    public static void main(String[] args) {
-
-            Scanner reader = new Scanner(System.in);
-            ArrayList<String> words = new ArrayList<String>();
-        while(true) {
-            System.out.print("Type a word: ");
-            String word = reader.nextLine();
-
-            if (word.isEmpty()) {
-                break;
-            } else {
-                words.add(word);
-            }
-        }
-            Collections.sort(words);
-
-            System.out.println("You typed the following words:");
-            for (String word : words) {
-                System.out.println(word);
-            }
-
+    public static int countItems(ArrayList<String> list) {
+        return list.size();
     }
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Hello");
+        list.add("Ciao");
+        list.add("Hei");
+        list.add("Howdy");
+
+        System.out.println("There are this many items in the list:");
+        System.out.println(countItems(list));
+        }
 }
