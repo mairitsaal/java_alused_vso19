95ab7329d25a439c0bbc16db2f8b4355fdb49d48
mairit.saal <mairit.saal@khk.ee>
2020-11-06 23:59:31 +0200

    Ex 60

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,27 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Random;
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner reader = new Scanner(System.in);
-        ArrayList<String> words = new ArrayList<String>();
-
-        while (true) {
+            Scanner reader = new Scanner(System.in);
+            ArrayList<String> words = new ArrayList<String>();
+        while(true) {
             System.out.print("Type a word: ");
             String word = reader.nextLine();
-            if (words.contains(word)) {
-                System.out.println("You gave the word " + word + " twice");
+
+            if (word.isEmpty()) {
+                break;
             } else {
                 words.add(word);
             }
         }
+            Collections.sort(words);
 
+            System.out.println("You typed the following words:");
+            for (String word : words) {
+                System.out.println(word);
+            }
 
     }
 }
