3f7edbb11bd76c9c8a21b280173690a6dfc9ee94
mairit.saal <mairit.saal@khk.ee>
2020-11-07 23:06:20 +0200

    Ex 77

--- src/Main.java
+++ src/Main.java
@@ -6,30 +6,33 @@ public class Main {
         DecreasingCounter counter = new DecreasingCounter(100);
 
         Menu cafee = new Menu();
+        LyyraCard card = new LyyraCard(50);
 
-        cafee.addMeal("Hamburger");
-        cafee.addMeal("Fish'n'Chips");
-        cafee.addMeal("Sauerkraut");
+        System.out.println(card);
+        card.payEconomical();
+        System.out.println(card);
+        card.payGourmet();
+        System.out.println(card);
+        card.loadMoney(300);
+        System.out.println(card);
 
-        cafee.printMeals();
-        cafee.printMeals();
-        cafee.printMeals();
-        cafee.clearMenu();
+        LyyraCard cardPekka = new LyyraCard(20);
+        LyyraCard cardBrian = new LyyraCard(30);
 
+        cardPekka.payGourmet();
+        cardBrian.payEconomical();
+        System.out.println("Pekka: The card has" + cardPekka+ " euros");
+        System.out.println("Brian: The card has" + cardBrian+ " euros");
+        cardPekka.loadMoney(20);
+        cardBrian.payGourmet();
+        System.out.println("Pekka: The card has" + cardPekka+ " euros");
+        System.out.println("Brian: The card has" + cardBrian+ " euros");
+        cardPekka.payEconomical();
+        cardPekka.payEconomical();
+        cardBrian.loadMoney(50);
+        System.out.println("Pekka: The card has" + cardPekka+ " euros");
+        System.out.println("Brian: The card has" + cardBrian+ " euros");
 
-        counter.printValue();
-
-        counter.decrease();
-        counter.printValue();
-
-        counter.decrease();
-        counter.printValue();
-
-        counter.reset();
-        counter.printValue();
-
-        counter.setInitial();
-        counter.printValue();
     }
 }
 
