f95466d88c1c35164e61c152bec7b5751160883b
mairit.saal <mairit.saal@khk.ee>
2020-11-07 21:02:06 +0200

    Ex 76

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,18 @@ public class Main {
     public static void main(String[] args) {
         DecreasingCounter counter = new DecreasingCounter(100);
 
+        Menu cafee = new Menu();
+
+        cafee.addMeal("Hamburger");
+        cafee.addMeal("Fish'n'Chips");
+        cafee.addMeal("Sauerkraut");
+
+        cafee.printMeals();
+        cafee.printMeals();
+        cafee.printMeals();
+        cafee.clearMenu();
+
+
         counter.printValue();
 
         counter.decrease();
