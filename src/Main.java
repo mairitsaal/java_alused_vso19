380aa5a08b8e4e7cd96aae4137193e01676d0575
mairit.saal <mairit.saal@khk.ee>
2020-11-08 16:55:54 +0200

    Ex 84

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,41 @@
 import java.util.*;
 
     public class Main {
-        public static void main(String[] args)  {
+        public static void main(String[] args) {
 
-                    PasswordRandomizer randomizer = new PasswordRandomizer(13);
-                    System.out.println("Password: " + randomizer.createPassword());
-                    System.out.println("Password: " + randomizer.createPassword());
-                    System.out.println("Password: " + randomizer.createPassword());
-                    System.out.println("Password: " + randomizer.createPassword());
-                }
-            }
+            Counter counter1 = new Counter(0, true);
+            Counter counter2 = new Counter(80);
+            Counter counter3 = new Counter(true);
+            Counter counter4 = new Counter();
+
+            counter1.increase();
+            counter2.increase();
+            counter3.increase();
+            counter4.increase();
+
+            counter1.decrease();
+            counter2.decrease();
+            counter3.decrease();
+            counter4.decrease();
+
+            counter1.decrease();
+            counter2.decrease();
+            counter3.decrease();
+            counter4.decrease();
+
+            counter1.increase(10);
+            counter2.increase(20);
+            counter3.increase(30);
+            counter4.increase(40);
+
+            counter1.decrease(20);
+            counter2.decrease(2);
+            counter3.decrease(3);
+            counter4.decrease(-10);
+
+            System.out.println(counter1);
+            System.out.println(counter2);
+            System.out.println(counter3);
+            System.out.println(counter4);
+        }
+    }
