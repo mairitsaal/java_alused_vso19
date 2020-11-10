5ec7055d8683167e86e6a8fdad0abe71213c6c6f
mairit.saal <mairit.saal@khk.ee>
2020-11-10 23:11:49 +0200

    Ex 101-1-2 Book and Library ArrayList

---
+++ src/Library.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Library {
+    private ArrayList<Book> library;
+
+    public Library() {
+        this.library = new ArrayList<Book>();
+    }
+
+    public void addBook(Book newBook){
+        this.library.add(newBook);
+    }
+
+    public void printBooks(){
+        for (Book book: this.library) {
+            System.out.println(book);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,18 @@ import java.util.*;
 
     public class Main {
         public static void main(String[] args) {
-            NightSky NightSky = new NightSky(8, 4);
-            NightSky.print();
-            System.out.println("Number of stars: " + NightSky.starsInLastPrint());
-            System.out.println("");
 
-            NightSky.print();
-            System.out.println("Number of stars: " + NightSky.starsInLastPrint());
+            Library Library = new Library();
+
+            Book cheese = new Book("Cheese Problems Solved", "Woodhead Publishing", 2007);
+            Library.addBook(cheese);
+
+            Book nhl = new Book("NHL Hockey", "Stanley Kupp", 1952);
+            Library.addBook(nhl);
+
+            Library.addBook(new Book("Battle Axes", "Tom A. Hawk", 1851));
+
+            Library.printBooks();
         }
 
     }
