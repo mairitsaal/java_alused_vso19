c143b07929a75448d69a1d95e956e6bcb346fa92
mairit.saal <mairit.saal@khk.ee>
2020-11-10 23:29:12 +0200

    Ex 101-3 Book and ArrayList search

--- src/Library.java
+++ src/Library.java
@@ -7,13 +7,44 @@ public class Library {
         this.library = new ArrayList<Book>();
     }
 
-    public void addBook(Book newBook){
+    public void addBook(Book newBook) {
         this.library.add(newBook);
     }
 
-    public void printBooks(){
-        for (Book book: this.library) {
+    public void printBooks() {
+        for (Book book : this.library) {
             System.out.println(book);
         }
+    }
+
+    public ArrayList<Book> searchByTitle(String title) {
+        ArrayList<Book> found = new ArrayList<Book>();
+
+        for (Book book : this.library) {
+            if (book.title().contains(title)) {
+                found.add(book);
+            }
+        }
+        return found;
+    }
+
+    public ArrayList<Book> searchByPublisher(String publisher) {
+        ArrayList<Book> found = new ArrayList<Book>();
+        for (Book book : this.library) {
+            if (book.publisher().contains(publisher)) {
+                found.add(book);
+            }
+        }
+        return found;
+    }
+
+    public ArrayList<Book> searchByYear(int year) {
+        ArrayList<Book> found = new ArrayList<Book>();
+        for (Book book : this.library) {
+            if(book.year() == year) {
+                found.add(book);
+            }
+        }
+        return found;
     }
 }
